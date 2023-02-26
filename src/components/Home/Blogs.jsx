3598d41f288d5b8d6eb31d4c4a762bbc168b6b6d
Usama <usamasaeed1028@gmail.com>
2023-02-27 03:16:45 +0500

    blogs done

--- src/components/Home/Blogs.jsx
+++ src/components/Home/Blogs.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import React from "react";
 import styles from "../../styles/Hero.module.css";
 import image from "../../assets/ourwork/design.png";
-import img1 from "../../assets/blogs/img1.png"
-import img2 from "../../assets/blogs/img2.png"
-import img3 from "../../assets/blogs/img3.png"
-import img4 from "../../assets/blogs/img4.png"
+import img1 from "../../assets/blogs/img1.png";
+import img2 from "../../assets/blogs/img2.png";
+import img3 from "../../assets/blogs/img3.png";
+import img4 from "../../assets/blogs/img4.png";
 
 const BLOGS = [
   {
@@ -47,13 +47,29 @@ const BLOGS = [
 
 const Blogs = () => {
   return (
-    <section className={`w-full bg-[#424248] px-[80px] `}>
+    <section className={`w-full bg-[#424248] h-[] px-[80px] `}>
       <div className={` img-container w-full relative `}>
         <Image src={image} alt="img" />
         <div className="absolute top-0 z-[11]  w-full  flex flex-col gap-y-[32px] items-cen ter justify-center ">
           <h1 className="text-[48px] font-oswald font-bold text-white text-center">
             Our Works
           </h1>
+        
+        <div className="flex gap-x-[32px] justify-center">
+          {BLOGS.map((data) => {
+            return (
+              <div className="w-[210px] h-[385px] bg-white flex flex-col gap-y-[18px]">
+                <div className="img-container w-[210px] h-[210px]">
+                  <Image src={data.img} alt="img" />
+                </div>
+                <div className="px-[18px]">
+                <span className="text-[16px] font-oswald font-bold">{data.title}</span>
+                <p className="text-[12px] font-jakarta text-[#000000] text-opacity-[50%] pt-[18px]">{data.content}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
         </div>
       </div>
     </section>
