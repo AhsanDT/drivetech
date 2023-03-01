2d0b7150412fadc0d9dc0b45b6a843ea85940631
Usama <usamasaeed1028@gmail.com>
2023-03-02 02:07:09 +0500

    updated code

--- src/components/Layout/MobileFooter.jsx
+++ src/components/Layout/MobileFooter.jsx
@@ -23,23 +23,23 @@ const MobileFooter = () => {
       <div className="flex flex-col gap-y-[17px]">
         <h3>Contact us</h3>
         <span className="font-normal text-white text-[14px]">
-          Email: <span className="text-[#4169E1]">info@drivetechnology.co</span>
+          Email: <span className="text-[#4169E1]"><a href="mailto:info@drivetechnology.co">info@drivetechnology.co</a></span>
         </span>
         <span className="font-light text-white text-[14px]">
-          Phone: <span>+1 (888) 910 8298 92188 </span>
+          Phone: <span><a href="tel:+1 (888) 910 8298 92188">+1 (888) 910 8298 92188 </a></span>
         </span>
       </div>
       <div className="border-b-[2px] border-b-[#A3A3A3]">
         <ul className="flex flex-col gap-y-[25px]">
           <Link href={"/services"}><li>Services</li></Link>
           <Link href={"/development"}><li>Development</li></Link>
-          <li>Reesources</li>
+          <Link href={"/blogs"}><li>Resources</li></Link>
           <li className="pb-[25px]">Legal</li>
         </ul>
       </div>
       <div className="flex justify-center gap-x-[40px]">
-        <h4>Terms of use</h4>
-        <h4>Privacy</h4>
+        <Link href={"/termsandconditions"}><h4>Terms of use</h4></Link>
+        <Link href={"/privacypolicy"}><h4>Privacy</h4></Link>
       </div>
       <div className="flex gap-x-2 justify-center items-center">
         <Link href={"/"}>
