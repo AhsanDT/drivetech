2c69f6690168245adcc26d70b09f7bdc6558daca
Usama <usamasaeed1028@gmail.com>
2023-03-02 18:05:46 +0500

    updated code

--- src/components/Home/Idea.jsx
+++ src/components/Home/Idea.jsx
@@ -3,15 +3,17 @@ import Button from '../UI/Button'
 
 const Idea = ({title}) => {
   return (
-    <div className="absolute bottom-0 w-full bg-[black] bg-opacity-[10%] lg:mt-[120px] flex flex-col gap-y-[14px] lg:flex-row justify-between lg:items-center px-[30px] lg:px-[214px] py-[38px]">
+    <div className="absolute bottom-0 w-full bg-[black] bg-opacity-[10%] lg:mt-[120px] flex flex-col gap-y-[14px] lg:flex-row justify-between   lg:items-center px-[30px] lg:px-[214px] py-[38px]">
       <div className="flex flex-col ">
         <span className="font-oswald text-[24px] lg:text-[50px] font-bold text-[#ffffff] uppercase">
         Have an idea?
         </span>
         <span className="font-oswald text-[24px] lg:text-[50px] font-light text-[#ffffff] uppercase">Tell us about it.</span>
       </div>
+      <div>
       <Button className="hidden md:block w-fit">{title}</Button>
       <Button className=" md:hidden w-fit">Start a Project</Button>
+      </div>
     </div>
   )
 }
--- src/components/StartProject/Project.jsx
+++ src/components/StartProject/Project.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { InlineWidget } from "react-calendly";
-
+import Idea from "../Home/Idea";
+import styles2 from "../../styles/Partnership.module.css"
 const Project = ({ BLOGS }) => {
   return (
-    <section className="w-full h-full top-[70px] lg:top-[100px]  relative px-[30px] lg:px-[130px] py-[50px] ">
-      <div className="w-full h-full">
+    <section className="w-full h-full top-[120px]  relative pb-[100px]">
+      <div className="w-full h-full px-[30px] lg:px-[130px]">
         <div className="flex flex-col gap-y-[20px] lg:flex-row justify-center items-center">
           <h1 className="text-[24px] font-oswald font-bold lg:w-[325px]">
             Book a call with us by selecting any of the dates that your are
@@ -15,6 +16,13 @@ const Project = ({ BLOGS }) => {
           </div>
         </div>
       </div>
+      
+          <div className="relative pt-[210px] lg:pt-[230px] bg-[#37373B]">
+            <div
+              className={`${styles2.box}  linear-grad  hidden md:block `}
+            ></div>
+            <Idea title="Start a Project" />
+          </div>
     </section>
   );
 };
