21c4c882e57b5b64308248f4e23943968267614d
Usama <usamasaeed1028@gmail.com>
2023-03-02 18:06:45 +0500

    updated code

--- src/components/StartProject/Project.jsx
+++ src/components/StartProject/Project.jsx
@@ -4,7 +4,7 @@ import Idea from "../Home/Idea";
 import styles2 from "../../styles/Partnership.module.css"
 const Project = ({ BLOGS }) => {
   return (
-    <section className="w-full h-full top-[120px]  relative pb-[100px]">
+    <section className="w-full h-full top-[120px]  relative pb-[120px]">
       <div className="w-full h-full px-[30px] lg:px-[130px]">
         <div className="flex flex-col gap-y-[20px] lg:flex-row justify-center items-center">
           <h1 className="text-[24px] font-oswald font-bold lg:w-[325px]">
