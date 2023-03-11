9371a976b79e4a203d67ac8b0f849a12f5055f9c
Usama <usamasaeed1028@gmail.com>
2023-03-11 06:10:49 +0500

    updated code

--- src/components/Home/Blogs.jsx
+++ src/components/Home/Blogs.jsx
@@ -58,7 +58,7 @@ const Blogs = () => {
             <Image src={image} alt="image" />
           </div>
           <div className="relative top-[40px] flex flex-col gap-y-[70px] px-[30px] md:px-[80px]">
-            <h1 className="text-[32px] lg:text-[48px] font-oswald font-bold text-white xl:text-center">
+            <h1 className="text-[32px] lg:text-[48px] font-oswald font-bold text-white xl:text-center pt-[50px] md:pt-0 ">
               Blogs
             </h1>
 
