e2d0bba48377587ea588d9442f38cafa60cde9e8
Usama <usamasaeed1028@gmail.com>
2023-04-13 08:40:17 +0500

    feat: button hover effect!

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Button = ({ children, className, onClick }) => {
   return (
     <button
-      className={`border bg-[#ffffff] px-[20px] py-[10px] text-black font-bold ${
+      className={`border bg-[#ffffff] px-[20px] py-[10px] text-black font-bold button ${
         className && className
       } `}
       onClick={() => (onClick ? onClick() : {})}
